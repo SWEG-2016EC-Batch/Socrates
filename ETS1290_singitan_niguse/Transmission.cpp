c12cd3c9287bba69b0ebf7b7e50270d4ee0f7921
Singitan Niguse <singitan.niguse@aastustudent.edu.et>
2024-12-01 11:32:07 +0300

    Update Transmission.cpp

--- ETS1290_singitan_niguse/Transmission.cpp
+++ ETS1290_singitan_niguse/Transmission.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	int days,hours,minutes,seconds;
+	int file_size;
+	int time;
+	int transmission_rate=960;
+	cout<<"enter the file size in bytes\n";
+	cin>>file_size;
+	time=file_size/transmission_rate;
+	days=time/86,400;
+	time=time%86,400;
+	hours=time/3600;
+	time=time%3600;
+	minutes=time/60;
+	time=time%60;
+	seconds=time;
+	cout<<"it takes "<<days<<" day:"<<hours<<" hour:"<<minutes<<" minute:"<<seconds<<" second";
+	return 0;
+}
