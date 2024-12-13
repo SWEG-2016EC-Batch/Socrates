d996d11caeae482dd530f78ce9961a14e38ced1e
Semlan Awel Draga <selman.awel@aastustudent.edu.et>
2024-12-13 22:33:12 +0300

    Update 3b.cpp

--- ETS1263_Selman_Awel/3b.cpp
+++ ETS1263_Selman_Awel/3b.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int rows;
+    char ch = 'A';
+
+    cout << "Enter the number of rows for the triangle: ";
+    cin >> rows;
+
+    for (int i = 0; i <= rows; i++) {
+        char printChar = ch;
+        // Print characters
+        for (int j = 0; j <rows; j++) {
+
+            cout << printChar++ << " ";
+
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
