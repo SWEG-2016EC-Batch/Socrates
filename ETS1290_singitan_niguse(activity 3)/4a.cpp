c9b73a0e898bdba2c4bc6bb71ef21872db104295
Singitan Niguse <singitan.niguse@aastustudent.edu.et>
2024-12-13 22:35:55 +0300

    Update 4a.cpp

--- ETS1290_singitan_niguse(activity 3)/4a.cpp
+++ ETS1290_singitan_niguse(activity 3)/4a.cpp
@@ -127,3 +127,25 @@ int main() {
 
     return 0;
 }
+//inverted_half_pyramid
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = rows; i >= 1; --i) {
+        for(int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
