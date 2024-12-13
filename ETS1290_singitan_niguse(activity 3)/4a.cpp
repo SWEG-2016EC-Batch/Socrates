073e1800004fbb1fda951ed7d60b6dfe31c01a76
Singitan Niguse <singitan.niguse@aastustudent.edu.et>
2024-12-13 22:20:58 +0300

    Update 4a.cpp

--- ETS1290_singitan_niguse(activity 3)/4a.cpp
+++ ETS1290_singitan_niguse(activity 3)/4a.cpp
@@ -14,3 +14,22 @@ int main()
     }
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() 
+
+{
+    
+    char ch ='A';
+    for (int i=0; i<5; i++)
+    {
+        for( int j=0; j<6; j++)
+        {
+            cout<<ch;
+            ch++;
+        }
+       cout<<endl;
+    }
+    return 0;
+}
