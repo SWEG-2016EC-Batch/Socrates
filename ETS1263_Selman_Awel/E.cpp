4a2e4f9caeff06a75dff6f49fe99b346912e5a53
Semlan Awel Draga <selman.awel@aastustudent.edu.et>
2024-12-13 20:56:05 +0300

    Update E.cpp

--- ETS1263_Selman_Awel/E.cpp
+++ ETS1263_Selman_Awel/E.cpp
@@ -1,1 +1,11 @@
+string num;
+char temp;
+cout<<"enter the number"<<endl;
+cin>>num;
+temp=num[0];
+num[0]=num[num.size()-1];
+num[num.size()-1]=temp;
+cout<<"number after swapping is  :"<<num<<endl;
+return 0;
+}
 
