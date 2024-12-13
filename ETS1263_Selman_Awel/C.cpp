fc50977989d8314c6b3dc4999855a2951c331137
Semlan Awel Draga <selman.awel@aastustudent.edu.et>
2024-12-13 20:51:51 +0300

    Update C.cpp

--- ETS1263_Selman_Awel/C.cpp
+++ ETS1263_Selman_Awel/C.cpp
@@ -1,1 +1,15 @@
+int num,digit,even=1;
+  cout<<"enter the number"<<endl;
+  cin>>num;
+  if(num<0){
+  num=-num;
+}while(num!=0){
+digit=num%10;
+if(digit%2==0){
+  even*=digit;
+  }
+num/=10;  
+}cout<<"the multiplication of even digits is "<<even<<endl;
+return 0;
+}
 
