f0ead5d28bc3905867fc7fa290ae89db0bffedf5
Singitan Niguse <singitan.niguse@aastustudent.edu.et>
2024-12-13 22:45:22 +0300

    Update 4b.cpp

--- ETS1290_singitan_niguse(activity 3)/4b.cpp
+++ ETS1290_singitan_niguse(activity 3)/4b.cpp
@@ -23,3 +23,27 @@ int main() {
 
     return 0;
 }
+//right_angle_triangle_to_right_with_alphabet
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    char ch = 'A';
+
+    cout << "Enter the number of rows for the triangle: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        char printChar = ch;
+        // Print characters
+        for (int j = 1; j <= i; j++) {
+
+            cout << printChar++ << " ";
+
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
