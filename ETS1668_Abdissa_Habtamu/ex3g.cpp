f0d7e90ce0757b305da5104d15c881ecde2cd78b
Abdissa Habtamu <abdissahabtamu27@gmail.com>
2024-12-13 22:03:02 +0300

    Update ex3g.cpp

--- ETS1668_Abdissa_Habtamu/ex3g.cpp
+++ ETS1668_Abdissa_Habtamu/ex3g.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+int digit,rev=0,num,temp;
+cout<<"enter the number"<<endl;
+cin>>num;
+temp=num;
+while(num!=0){
+  digit=num%10;
+  rev=(rev*10)+digit;
+  num/=10;
+}cout<<"the reverse of "<<temp<<" is "<<rev<<endl;
+if(temp==rev){
+  cout<<"the number is palindrome\n";
+}else{
+cout<<"the number is not palindrome\n";  
+}
+return 0;
+}
