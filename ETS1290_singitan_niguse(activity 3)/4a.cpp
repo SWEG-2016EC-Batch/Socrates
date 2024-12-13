ab29f485fc4dee8f765dee0c957995ab5836fb14
Singitan Niguse <singitan.niguse@aastustudent.edu.et>
2024-12-13 22:40:00 +0300

    Update 4a.cpp

--- ETS1290_singitan_niguse(activity 3)/4a.cpp
+++ ETS1290_singitan_niguse(activity 3)/4a.cpp
@@ -174,3 +174,31 @@ int main() {
 
     return 0;
 }
+//hollow_rectangle
+#include <iostream>
+using namespace std;
+
+int main() 
+
+{
+int rows, cols;
+    cout<<"RECTANGLE "<<endl;
+    cout<<" Enter the numbr of rows fo the rectangle:";
+cin>>rows;
+cout<<"Enter the number of coloums for the rectangle:";
+cin>>cols;
+    for(int i = 1; i <=rows; i++) {
+        for(int j = 1; j <= cols; j++) {
+            if (  i==1 || i==rows || j==1 || j==cols)
+            {
+               cout <<"*"<<"";
+            }
+            else
+            {
+                  cout<<" ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
