f71fe5e55bdbe5dbae4b7c3eb47ce103b0972015
Singitan Niguse <singitan.niguse@aastustudent.edu.et>
2024-12-13 22:29:57 +0300

    Update 4a.cpp

--- ETS1290_singitan_niguse(activity 3)/4a.cpp
+++ ETS1290_singitan_niguse(activity 3)/4a.cpp
@@ -73,3 +73,31 @@ int main() {
     }    
     return 0;
 }
+
+//inverted_half_pyramid
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This program prints an inverted hollow half pyramid with a vertical and diagonal line
+    int rows;
+    cout << "Enter the number of rows for the inverted half pyramid: ";
+    cin >> rows;
+    
+    for (int i = 0; i < rows; i++) {
+        // Print stars and spaces for hollow part
+        for (int j = i; j < rows; j++) {
+            if (i == 0 || j == i || j == rows - 1) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        
+        cout << endl;
+    }
+    
+   
+    return 0;
+}
