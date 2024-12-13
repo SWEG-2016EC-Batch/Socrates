4dcac17e301ba7e0d03ccd284363ad6fbadb3f0f
Singitan Niguse <singitan.niguse@aastustudent.edu.et>
2024-12-13 19:52:37 +0300

    Update E.cpp

--- ETS1290_singitan_niguse(activity 3)/E.cpp
+++ ETS1290_singitan_niguse(activity 3)/E.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+string num;
+char temp;
+cout<<"enter the number"<<endl;
+cin>>num;
+temp=num[0];
+num[0]=num[num.size()-1];
+num[num.size()-1]=temp;
+cout<<"number after swapping is  :"<<num<<endl;
+return 0;
+}
