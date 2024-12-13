2e0be0daaedc93e951f270401640ac23987a00b5
Semlan Awel Draga <selman.awel@aastustudent.edu.et>
2024-12-13 20:53:50 +0300

    Update D.cpp

--- ETS1263_Selman_Awel/D.cpp
+++ ETS1263_Selman_Awel/D.cpp
@@ -1,1 +1,16 @@
+int num,first,last;
+cout<<"enter the number"<<endl;
+  cin>>num;
+  
+  if(num<0){
+  num=-num;
+}
+last=num%10;
+first=num;
+while(first>=10){
+  first/=10;
+}
+cout<<"the first number is "<<first<<" the last number is "<<last<<" their sum is "<<first+last<<endl;
+return 0;
+}
 
