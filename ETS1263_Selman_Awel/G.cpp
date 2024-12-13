5ce14ce4bd27601b4b40f2dbb7f1c0c814e8f811
Semlan Awel Draga <selman.awel@aastustudent.edu.et>
2024-12-13 20:57:48 +0300

    Update G.cpp

--- ETS1263_Selman_Awel/G.cpp
+++ ETS1263_Selman_Awel/G.cpp
@@ -1,1 +1,17 @@
+int digit,rev=0,num,temp;
+cout<<"enter the number"<<endl;
+cin>>num;
+temp=num;
+while(num!=0){
+  digit=num%10;
+  rev=(rev*10)+digit;
+  num/=10;
+}cout<<"the reverse of "<<temp<<" is "<<rev<<endl;
+if(temp==rev){
+  cout<<"the number is palindrome\n";
+}else{
+cout<<"the number is not palindrome\n";  
+}
+return 0;
+}
 
